211fa8b91c405275ffe9bc1f51233a3394bbe064
rberman6 <rittuberman@gmail.com>
2023-05-21 12:24:12 -0600

    created development branch, header stateless component and tested API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import axios from 'axios';
+import { useEffect } from 'react';
+
+// PSEUDO CODE
+// 1. Page loads/mounts with button "Upcoming Movies" and separate dropdown menu nested inside form to filter movies by genre.
+// 2. User clicks on button which runs an event
+// 3. This event vent makes API call
+// 4. API movie data returned will be stored in state
+// 5. API data (poster, title, short blurb, release date) will render on page
+// 7. User can filter movie data by genre
+// 8. users selects genre from drop down menu and click on submit. an onChange function runs to filter the movies based on users selected genre.
+// 9. User has option to clear the movie data
 
 function App() {
+  // create a function that gets Movie data (poster, title etc.)
+  useEffect(() => {
+    const apiKey = `4d17ba0490e1fa0dcff61ffcfe3e0764`;
+    axios ({
+      // url: "https://api.themoviedb.org/3/movie/top_rated",
+      url: "https://api.themoviedb.org/3/movie/upcoming",
+      method: "GET",
+      dataResponse: "json",
+      params: {
+        api_key: apiKey,
+        format: "json",
+        language: 'en-US',
+        page: 1,
+      },
+    }).then((dataApi) => {
+      console.log(dataApi);
+      console.log(dataApi.data.results);
+     
+    });
+  });
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="wrapper">
+      <Header />
+    </main>
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+// this component will be stateless
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Movie App</h1>
+            <h2>Catchy tagline</h2>
+        </header>
+    )
+};
+
+export default Header;
