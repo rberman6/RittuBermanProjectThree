a023fabd2fba5152e59ea354bad75838aa9b1b80
rberman6 <rittuberman@gmail.com>
2023-06-06 10:58:40 -0600

    minor adjustments

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
         <header id="scrollUp">
             <h1 className="mainHeading">Premiere Peek</h1> 
             <div className="rightToLeft">
-                <h2 className="tagline"><span> <svg id="a" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 216.4 204" width="9px" height="9px"><polygon points="176.7 202.6 109.6 168.6 43.3 204 54.8 130.6 0 78.9 75 67.3 107.5 0 141.4 66.6 216.4 76.2 163 129.2 176.7 202.6" fill="#100630" /></svg></span>Stay Ahead with Upcoming Movies! <span><svg id="a" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 216.4 204" width="9px" height="9px"><polygon points="176.7 202.6 109.6 168.6 43.3 204 54.8 130.6 0 78.9 75 67.3 107.5 0 141.4 66.6 216.4 76.2 163 129.2 176.7 202.6" fill="#100630" /></svg></span></h2>
+                <h2 className="tagline"><span><svg id="a" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 216.4 204" width="9px" height="9px"><polygon points="176.7 202.6 109.6 168.6 43.3 204 54.8 130.6 0 78.9 75 67.3 107.5 0 141.4 66.6 216.4 76.2 163 129.2 176.7 202.6" fill="#100630" /></svg></span>Stay Ahead with Upcoming Movies! <span><svg id="a" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 216.4 204" width="9px" height="9px"><polygon points="176.7 202.6 109.6 168.6 43.3 204 54.8 130.6 0 78.9 75 67.3 107.5 0 141.4 66.6 216.4 76.2 163 129.2 176.7 202.6" fill="#100630" /></svg></span></h2>
             </div>
         </header>
     )
